66558a367d414ec8e1404abec76a3d9bf4f19375
Soham Adwani <sohamthenerd@gmail.com>
2024-06-09 20:13:36 +0700

    added post paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://snazzyham.com',
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind({
-      applyBaseStyles: false
-    })
-  ],
-  redirects: {
-    '/post/[...slug]': '/writing/[...slug]'
-  }
+	site: "https://snazzyham.com",
+	integrations: [
+		mdx(),
+		sitemap(),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
+	redirects: {
+		"/post/[slug]": "/writing/[slug]",
+	},
 });
