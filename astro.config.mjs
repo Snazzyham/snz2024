4d1c2398a74350fcccaf796645fccc680b032aaf
Soham Adwani <sohamthenerd@gmail.com>
2025-07-31 13:23:36 +0700

    sitemap config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://snazzyham.com",
-	integrations: [
-		mdx(),
-		sitemap(),
-		tailwind({
-			applyBaseStyles: false,
-		}),
-	],
-	redirects: {
-		"/post/[slug]": "/writing/[slug]",
-	},
+  site: 'https://snazzyham.com',
+  integrations: [
+    mdx(),
+    sitemap({
+      filter: (page) => !page.includes('/post/')
+    }),
+    tailwind({
+      applyBaseStyles: false
+    })
+  ],
+  redirects: {
+    '/post/[slug]': '/writing/[slug]'
+  }
 });
